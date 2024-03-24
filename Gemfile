9c0de6dd0b2e4616ba11d4e2c0e3d698276a7a9d
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-03-24 11:27:32 +0300

    add gem Faker

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
+
+  gem "faker", "~> 3.2.3"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+30.times do
+  title = Faker::Book.title
+  summary = Faker::Lorem.paragraph(sentence_count: 4, supplemental: true, random_sentences_to_add: 4) 
+  price = Faker::Number.decimal(l_digits: 3, r_digits: 2)
+
+  Product.create title: title, summary: summary, price: price
+end
