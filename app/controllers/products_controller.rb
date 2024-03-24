7e036272e5a4c1663c20acc31ca318d0bf1a952a
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-03-24 09:59:15 +0300

    add method new/create Product, form, store index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params_product)
+
+    if @product.save
+      flash[:success] = "Object successfully created"
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def params_product
+    params.require(:product).permit(:title, :summary, :price)
+  end
+  
 end
